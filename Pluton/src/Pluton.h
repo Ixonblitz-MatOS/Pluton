d8ddd8c82921dd6c0aa75c51bc9ac866a416f48e
Matthew Gavell <mgavell@IT232.Local>
2022-10-21 10:46:11 -0400

    Premake added,logging

--- Pluton/src/Pluton.h
+++ Pluton/src/Pluton.h
@@ -1,8 +1,11 @@
 #pragma once
 
 //For use by Pluton applications
-
+#include <stdio.h>
 #include "Pluton/Application.h"
 
 //entry point
 #include "Pluton/EntryPoint.h"
+
+//logging
+#include "Pluton/Log.h"
--- Pluton/src/Pluton/EntryPoint.h
+++ Pluton/src/Pluton/EntryPoint.h
@@ -1,13 +1,14 @@
 #pragma once
-
+#include "Log.h"
 
 #ifdef PL_PLATFORM_WINDOWS
 extern Pluton::Application* Pluton::CreateApplication();
 int main(int argc, char** argv) {
+	Pluton::Log::Init();
+	PL_CORE_INFO("Initialized Core Logger!");
+	PL_CORE_INFO("Initialized Client Logger");
 	auto app = Pluton::CreateApplication();
 	app->Run();
 	delete app;
 }
-#else
-	#error Pluton only supports Windows!
 #endif
